89569086b586d0e97c409207c5cdd665aaf9d8a5
David Rygwelski <drygw01@gmail.com>
2020-05-11 16:08:41 -0400

    Done with Explorer Mode.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace our_dotnet_app
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      Console.WriteLine("Hello, my name is Dave");
     }
   }
 }
